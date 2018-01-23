8691935af6b1f2af0221c961afafb6bf33247171
aleksandr <alx.panteleev@gmail.com>
2018-01-23 21:03:08 +0300

    refactor routes to route- config

--- src/client/routes.js
+++ src/client/routes.js
@@ -1,11 +1,15 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
 import Home from './components/home'
 import UsersList from './components/users-list';
 
-export const Routes = () => (
-    <div>
-        <Route exact path="/" component={Home} />
-        <Route path="/users" component={UsersList} />
-    </div>
-);
+export const Routes = [
+    {
+        path: '/',
+        component: Home,
+        exact: true
+    },
+    {
+        path: '/users',
+        component: UsersList
+    }
+];
