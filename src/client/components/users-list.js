fd0b062ecc7ce1d745a0fd44892b27d6cca1e1d5
aleksandr <alx.panteleev@gmail.com>
2018-01-23 20:22:58 +0300

    wire up

--- src/client/components/users-list.js
+++ src/client/components/users-list.js
@@ -5,25 +5,28 @@ import { fetchUsers } from '../actions';
 
 class UsersList extends Component {
     componentDidMount () {
-        // this.props.fetchUsers()
+        this.props.fetchUsers()
     }
 
     renderUsers () {
-        this.props.users.map((user) => <li key={user.id}>{user.name}</li>)
-    }
+        return this.props.users.map((user) => {
+            return <li key={user.id}>{user.name}</li>
+        })
+    };
 
     render () {
         return (
             <div>
                 List of users:
-                <ul>{this.renderUsers()}</ul>
+                <ul>
+                    {this.renderUsers()}
+                </ul>
             </div>
         )
     }
 }
 
 const mapStateToProps = ({ users }) => ({ users });
-const mapDispatchToProps = (dispatch) => ({ fetchUsers: dispatch(fetchUsers) });
 
-export default connect(mapStateToProps, mapDispatchToProps)(UsersList);
+export default connect(mapStateToProps, { fetchUsers })(UsersList);
 
--- src/client/routes.js
+++ src/client/routes.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Home from './components/home'
+import UsersList from './components/users-list';
 
 export const Routes = () => (
     <div>
         <Route exact path="/" component={Home} />
+        <Route path="/users" component={UsersList} />
     </div>
 );
