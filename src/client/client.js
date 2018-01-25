894bbf35e3ccb7f9130ebaf9fb03b8e4f6ce3896
aleksandr <alx.panteleev@gmail.com>
2018-01-25 22:58:27 +0300

    axios instance and redux thunk 3rd argument

--- src/client/client.js
+++ src/client/client.js
@@ -5,14 +5,19 @@ import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
 import { renderRoutes } from 'react-router-config';
+import axios from 'axios';
 
 import { routes } from './routes';
 import { BrowserRouter } from 'react-router-dom';
 import reducers from './reducers'
 
+const axiosInstance = axios.create({
+    baseURL: '/api'
+});
+
 const initialState = window.initialState;
 
-const store = createStore(reducers, initialState, applyMiddleware(thunk));
+const store = createStore(reducers, initialState, applyMiddleware(thunk.withExtraArgument(axiosInstance)));
 
 ReactDom.hydrate(
     <Provider store={store}>
