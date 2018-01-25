aa7d0a4daff3839a45f62625b6d66f5dab5adf74
aleksandr <alx.panteleev@gmail.com>
2018-01-25 22:42:41 +0300

    proxy setup

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import 'babel-polyfill';
 import express from 'express';
 import { matchRoutes } from 'react-router-config';
+import proxy from 'express-http-proxy';
 
 import renderer from './helpers/renderer';
 import createStore from './helpers/createStore';
@@ -8,17 +9,24 @@ import { routes } from './client/routes';
 
 const app = express();
 
-app.use(express.static('public'));
-
 const PORT = 3000;
 
+app.use(
+    '/api',
+    proxy('http://react-ssr-api.herokuapp.com', {
+        proxyReqOptDecorator (opts) {
+            opts.header['x-forwarded-host'] = `localhost:${PORT}`;
+            return opts;
+        }
+    }));
+app.use(express.static('public'));
 app.get('*', (req, res) => {
     const store = createStore();
     const promises = matchRoutes(routes, req.path).map(({ route }) => {
         return route.loadData ? route.loadData(store) : null;
     });
 
-    Promise.all(promises).then(()=> {
+    Promise.all(promises).then(() => {
         res.send(renderer(req, store));
     })
 });
