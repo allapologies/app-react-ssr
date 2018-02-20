985bf9c97b529f176ca99bdb3cf4ea294ab5b03c
Aleksandr Panteleev <alx.panteleev@gmail.com>
2018-02-20 21:23:08 +0300

    handle promise.all errors

--- src/index.js
+++ src/index.js
@@ -22,9 +22,17 @@ app.use(
 app.use(express.static('public'));
 app.get('*', (req, res) => {
     const store = createStore(req);
-    const promises = matchRoutes(routes, req.path).map(({ route }) => {
-        return route.loadData ? route.loadData(store) : null;
-    });
+    const promises = matchRoutes(routes, req.path)
+        .map(({ route }) => {
+            return route.loadData ? route.loadData(store) : null;
+        })
+        .map((promise) => {
+            if (promise) {
+                return new Promise((resolve, /**reject**/) => {
+                    promise.then(resolve).catch(resolve)
+                })
+            }
+        })
 
     Promise.all(promises).then(() => {
         const context = {};
